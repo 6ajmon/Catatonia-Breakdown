17ff26f29fd2472f7f218eb4284ec1bc6a12b595
6ajmon <6ajmon@gmail.com>
2025-10-10 13:34:46 +0200

    Add temporary player and automatic camera change

--- Source/Tmp/Level/Level.cs
+++ Source/Tmp/Level/Level.cs
@@ -7,4 +7,20 @@ public partial class Level : Node3D
     {
         CameraManager.Instance.GetCameras();
     }
+    private void OnArea1Entered(Node3D body)
+    {
+        if (body is CharacterTmp)
+        {
+            GD.Print("Entered Area 1");
+            CameraManager.Instance.OnChangeCamera(1);
+        }
+    }
+    private void OnArea2Entered(Node3D body)
+    {
+        if (body is CharacterTmp)
+        {
+            GD.Print("Entered Area 2");
+            CameraManager.Instance.OnChangeCamera(0);
+        }
+    }
 }
