2454f037eafc3486cf50cdb06a44c039446045f0
michal <michal.now03@gmail.com>
2025-10-13 00:00:50 +0200

    first and third person movement

--- Source/Player/Novak/NovakPlayer.cs
+++ Source/Player/Novak/NovakPlayer.cs
@@ -5,7 +5,14 @@ public partial class NovakPlayer : CharacterBody3D
 {
     [Export] public float Speed = 2.0f;
     [Export] public float RunSpeed = 3.0f;
+    [Export] public float RotationSpeed = 2.0f;
+    [Export] public float RunRotationSpeed = 3.0f;
+    [Export] public float MouseSensitivity = 0.16f;
+    [Export] public float MaxCameraAngle = 60.0f;
+    Vector3 direction = Vector3.Zero;
     private bool _isRunning = false;
+    private bool _movingBackwards = false;
+    private bool isFirstPerson = false;
     [Export] public AnimationPlayer animationPlayer;
     [Export] public AnimationTree animationTree;
     [Export] public StateMachine stateMachine;
@@ -22,46 +29,20 @@ public partial class NovakPlayer : CharacterBody3D
     }
     public override void _Process(double delta)
     {
-        if (firstPersonCamera.Current)
+        isFirstPerson = firstPersonCamera.Current;
+        if (isFirstPerson)
         {
-            skeleton.Visible = false;
+            Input.MouseMode = Input.MouseModeEnum.Captured;
         }
         else
         {
-            skeleton.Visible = true;
+            Input.MouseMode = Input.MouseModeEnum.Visible;
         }
-        Vector3 direction = Vector3.Zero;
-
-        if (Input.IsActionPressed("MoveUp"))
-            direction -= Transform.Basis.Z;
-        if (Input.IsActionPressed("MoveDown"))
-            direction += Transform.Basis.Z;
-        if (Input.IsActionPressed("MoveLeft"))
-            direction -= Transform.Basis.X;
-        if (Input.IsActionPressed("MoveRight"))
-            direction += Transform.Basis.X;
-
-        direction = direction.Normalized();
-        _isRunning = Input.IsActionPressed("RunToggle");
-        var speed = _isRunning ? RunSpeed : Speed;
-        Velocity = direction * speed;
-        if (stateMachine != null)
-        {
-            if (direction == Vector3.Zero)
-            {
-                stateMachine.ChangeState(stateMachine.GetNode<PlayerIdleState>("Idle"));
-            }
-            else if (_isRunning)
-            {
-                stateMachine.ChangeState(stateMachine.GetNode<PlayerRunningState>("Running"));
-            }
-            else
-            {
-                stateMachine.ChangeState(stateMachine.GetNode<PlayerWalkingState>("Walking"));
-            }
-        }
+        
+        updateInput();
+        updateStateMachine();
         HandleAnimation(delta);
-        MoveAndSlide();
+        skeleton.Visible = !isFirstPerson;
     }
 
     public void HandleAnimation(double delta)
@@ -90,6 +71,93 @@ public partial class NovakPlayer : CharacterBody3D
         {
             animationTree.Set("parameters/Walk/blend_amount", _walkBlendAmount);
             animationTree.Set("parameters/Run/blend_amount", _runBlendAmount);
+        }
+    }
+
+    public void updateInput()
+    {
+        direction = Vector3.Zero;
+        _movingBackwards = false;
+        _isRunning = Input.IsActionPressed("RunToggle");
+        var rotationSpeed = _isRunning ? RunRotationSpeed : RotationSpeed;
+        
+        if (Input.IsActionPressed("MoveUp"))
+            direction += Transform.Basis.Z;
+        if (Input.IsActionPressed("MoveDown"))
+        {
+            direction -= Transform.Basis.Z;
+            _movingBackwards = true;
+        }
+        
+        if (!isFirstPerson)
+        {
+            if (Input.IsActionPressed("MoveLeft"))
+                RotateY(Mathf.DegToRad(rotationSpeed)); 
+            if (Input.IsActionPressed("MoveRight"))
+                RotateY(Mathf.DegToRad(-rotationSpeed));
+            
+        }
+        else
+        {
+            if (Input.IsActionPressed("MoveLeft"))
+            {
+                direction += Transform.Basis.X;
+            }
+
+            if (Input.IsActionPressed("MoveRight"))
+            {
+                direction -= Transform.Basis.X;
+            }
+
+            
+        }
+        
+        direction = direction.Normalized();
+        
+        var  speed = _isRunning ? RunSpeed : Speed;;
+        if (_movingBackwards)
+        {
+            _isRunning = false;
+            speed = Speed;
+        }
+        
+        Velocity = direction * speed;
+        MoveAndSlide();
+    }
+
+    public void updateStateMachine()
+    {
+        if (stateMachine != null)
+        {
+            if (direction == Vector3.Zero)
+            {
+                stateMachine.ChangeState(stateMachine.GetNode<PlayerIdleState>("Idle"));
+            }
+            else if (_isRunning)
+            {
+                stateMachine.ChangeState(stateMachine.GetNode<PlayerRunningState>("Running"));
+            }
+            else
+            {
+                stateMachine.ChangeState(stateMachine.GetNode<PlayerWalkingState>("Walking"));
+            }
+        }
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (isFirstPerson)
+        {
+            if (@event is InputEventMouseMotion mouseMotion)
+            {
+                RotateY(Mathf.DegToRad(-mouseMotion.Relative.X * MouseSensitivity));
+                float newX = firstPersonCamera.RotationDegrees.X - mouseMotion.Relative.Y * MouseSensitivity;
+                newX = Mathf.Clamp(newX, -MaxCameraAngle, MaxCameraAngle);
+                var rot = firstPersonCamera.RotationDegrees;
+                rot.X = newX;
+                firstPersonCamera.RotationDegrees = rot;
+            }
+            
         }
     }
 }
