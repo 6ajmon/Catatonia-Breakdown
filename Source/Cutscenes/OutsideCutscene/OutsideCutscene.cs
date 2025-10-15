cc41a0239182bb0ec6d22d5c15dab55dd22b14d1
6ajmon <6ajmon@gmail.com>
2025-10-15 21:01:43 +0200

    Set up cameras on outside cutscene

--- Source/Cutscenes/OutsideCutscene/OutsideCutscene.cs
+++ Source/Cutscenes/OutsideCutscene/OutsideCutscene.cs
@@ -4,6 +4,11 @@ using System.Threading.Tasks;
 
 public partial class OutsideCutscene : Cutscene
 {
+    public override void _Ready()
+    {
+        CameraManager.Instance.GetStaticCameras();
+        base._Ready();
+    }
     public override async Task RunSequence()
     {
         await Task.CompletedTask;
