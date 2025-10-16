b4621f13289fef044e7fd596ee2c04ccc10bc237
6ajmon <6ajmon@gmail.com>
2025-10-16 23:10:41 +0200

    Improve main menu cutscene

--- Source/Cutscenes/MainMenuCutscene/MainMenuCutscene.cs
+++ Source/Cutscenes/MainMenuCutscene/MainMenuCutscene.cs
@@ -16,8 +16,9 @@ public partial class MainMenuCutscene : Cutscene
         GameManager.Instance.IsCutscenePlaying = true;
         if (_animationPlayer != null)
         {
+            await WaitForSeconds(0.5f);
             _animationPlayer.Play("OpenGate");
-            await WaitForSeconds(2.0f);
+            await WaitForSeconds(2.5f);
             SignalManager.Instance.EmitSignal(nameof(SignalManager.CutsceneEnded));
         }
         
