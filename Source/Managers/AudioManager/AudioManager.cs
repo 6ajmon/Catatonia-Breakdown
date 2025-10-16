03fc72d13588d980c86b13165fb23212df4c6c2a
6ajmon <6ajmon@gmail.com>
2025-10-16 22:45:38 +0200

    Add methods to play sound

--- Source/Managers/AudioManager/AudioManager.cs
+++ Source/Managers/AudioManager/AudioManager.cs
@@ -1,6 +1,37 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class AudioManager : Node
 {
+    public static AudioManager Instance => ((SceneTree)Engine.GetMainLoop()).Root.GetNode<AudioManager>("AudioManager");
+    [Export] public AudioStreamPlayer MusicPlayer;
+    [Export] public AudioStreamPlayer SFXPlayer;
+
+    public void PlayMusic(AudioStream music)
+    {
+        if (MusicPlayer == null) return;
+        MusicPlayer.Stream = music;
+        MusicPlayer.Play();
+    }
+    public void PlaySFX(AudioStream sfx)
+    {
+        if (SFXPlayer == null) return;
+        SFXPlayer.Stream = sfx;
+        SFXPlayer.Play();
+    }
+
+    public void CreateAudioOneShotAtPosition(AudioStream sfx, Vector3 position, string bus = "SFX", float volumeDb = 0.0f)
+    {
+        var audioPlayer = new AudioStreamPlayer3D();
+        audioPlayer.Stream = sfx;
+        audioPlayer.GlobalPosition = position;
+        audioPlayer.Autoplay = false;
+        audioPlayer.Bus = bus;
+        audioPlayer.VolumeDb = volumeDb;
+        GetTree().Root.AddChild(audioPlayer);
+        audioPlayer.Play();
+        audioPlayer.Finished += () => audioPlayer.QueueFree();
+    }
+
 }
